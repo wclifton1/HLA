e1ce6ad9d72276f6c0dca4d8e939eeeb1449b7f1
Will Clifton <wclifton1@gmail.com>
2015-09-07 16:32:10 -0500

    background ideas

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,7 @@
 var wrap = $("#wrap");
 var window_height = $(window).height();
 
-wrap.on("scroll", function(e) {
+wrap.on("scroll", function() {
     
   if (this.scrollTop > window_height) {
     wrap.addClass("fix-nav");
--- js/min/custom-min.js
+++ js/min/custom-min.js
@@ -1,1 +1,1 @@
-var wrap=$("#wrap"),window_height=$(window).height();wrap.on("scroll",function(a){this.scrollTop>window_height?wrap.addClass("fix-nav"):wrap.removeClass("fix-nav")});
+var wrap=$("#wrap"),window_height=$(window).height();wrap.on("scroll",function(){this.scrollTop>window_height?wrap.addClass("fix-nav"):wrap.removeClass("fix-nav")});
