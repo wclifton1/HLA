21d6f79a8eb2dbc58b774363aa6c1e5b59310000
Will Clifton <wclifton1@gmail.com>
2015-09-20 22:20:37 -0500

    tidy up jquery and interface buttons

--- js/custom.js
+++ js/custom.js
@@ -10,3 +10,19 @@ wrap.on("scroll", function() {
   }
 });
 
+$('.text').hide();
+$('.text#multitasking').show();
+
+$('.phone-base div.btn-floating').click(function(){
+    var target = "#" + $(this).data("target");
+    console.log("target: %s", target);
+    $(".text").not(target).hide();
+    $(target).show();
+});
+
+$('.phone-text-panel span.js-phone-trigger').click(function(){
+    var target = "#" + $(this).data("target");
+    console.log("target: %s", target);
+    $(".text").not(target).hide();
+    $(target).show();
+});
